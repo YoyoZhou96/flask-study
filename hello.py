fdd4393314a2757bee7c9dc7bac6fc4767988c43
YoyoZhou96 <chouchou96@163.com>
2020-08-18 11:00:14 +0800

    使用flask-moment本地化时间和日期，配置表单，把表单渲染成HTML

--- hello.py
+++ hello.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+moment = Moment(app)
 bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -19,3 +31,8 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
